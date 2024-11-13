02abced1c6fd0678a1c8ca7b9642364a074ba9d9
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2024-11-13 23:32:49 +0530

    feat: Completed crud invoices feature

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,31 @@
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import EditInvoiceForm from "@/app/ui/invoices/edit-form";
+import { notFound } from "next/navigation";
+
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+  if (!invoice) {
+    notFound();
+  }
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: `/dashboard/invoices` },
+          {
+            label: "Edit Invoices",
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <EditInvoiceForm invoice={invoice} customers={customers} />
+    </main>
+  );
+}
